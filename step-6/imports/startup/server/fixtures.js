1ca7e4ab49eb4d6b49e15b6d30180a2452024261
nodexperts <development@nodexperts.com>
2017-07-16 00:06:54 +0530

    Step-6 added

---
+++ step-6/imports/startup/server/fixtures.js
@@ -0,0 +1,2 @@
+import Users from '../../api/users/users.js';
+import Posts from '../../api/posts/posts.js';
---
+++ step-6/imports/ui/pages/post/post.js
@@ -0,0 +1,28 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
+import Posts from './../../../api/posts/posts';
+import './../../layouts/loading.html';
+import './post.html';
+
+Template.post.onRendered = function () {
+};
+
+Template.post.helpers({
+  posts() {
+    return Posts.find();
+  },
+  isLiked(userIds) {
+    return userIds.indexOf(Meteor.userId()) >= 0;
+  },
+});
+
+Template.post.events({
+  'click #like': function () {
+    Meteor.call('posts.like', this._id, Meteor.userId());
+  },
+  'click #unlike': function () {
+    Meteor.call('posts.unlike', this._id, Meteor.userId());
+  },
+});
